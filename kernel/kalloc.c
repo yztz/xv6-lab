d38ac862603d48059ce8f194d4d5bc2a50a77160
yztz <781089956@qq.com>
2022-03-10 03:44:18 -0800

    lab:system call

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -80,3 +80,17 @@ kalloc(void)
     memset((char*)r, 5, PGSIZE); // fill with junk
   return (void*)r;
 }
+
+uint64
+freemem(void) {
+  struct run *r;
+  uint size = 0;
+
+  r = kmem.freelist;
+  while(r) {
+    size += 4096;
+    r = r->next;
+  }
+  
+  return size;
+}
--- kernel/proc.c
+++ kernel/proc.c
@@ -288,6 +288,7 @@ fork(void)
     return -1;
   }
   np->sz = p->sz;
+  np->trace_mask = p->trace_mask;
 
   // copy saved user registers.
   *(np->trapframe) = *(p->trapframe);
@@ -653,4 +654,18 @@ procdump(void)
     printf("%d %s %s", p->pid, state, p->name);
     printf("\n");
   }
+}
+
+
+uint64
+nproc(void)
+{
+  struct proc *p;
+  uint64 cnt = 0;
+
+  for(p = proc; p < &proc[NPROC]; p++){
+    if(p->state != UNUSED) cnt++;
+  }
+
+  return cnt;
 }
